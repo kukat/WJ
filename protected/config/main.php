f8b57883c4ccdada24d5a0648e1f015ad194fa29
Alex Yao <kukathe@gmail.com>
2011-08-12 01:41:55 +0800

    web app name & menu link to purchasing

--- protected/config/main.php
+++ protected/config/main.php
@@ -7,7 +7,7 @@
 // CWebApplication properties can be configured here.
 return array(
 	'basePath'=>dirname(__FILE__).DIRECTORY_SEPARATOR.'..',
-	'name'=>'My Web Application',
+	'name'=>'Purchasing Management System',
 
 	// preloading 'log' component
 	'preload'=>array('log'),
